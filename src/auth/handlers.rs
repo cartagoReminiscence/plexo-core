973f874d2ba9ed2b4cf6f8cdda8442ea79f21138
Bregy Malpartida <bregy@minsky.cc>
2024-02-23 12:23:41 -0500

    feat: update plexo-core and plexo-sdk versions

--- src/auth/handlers.rs
+++ src/auth/handlers.rs
@@ -7,21 +7,18 @@ use plexo_sdk::resources::members::extensions::{
 use plexo_sdk::resources::members::member::Member;
 
 use poem::http::header::{CACHE_CONTROL, EXPIRES, LOCATION, PRAGMA, SET_COOKIE};
-use poem::http::{HeaderMap, StatusCode};
+use poem::http::StatusCode;
 use poem::web::cookie::{Cookie, SameSite};
 use poem::web::{Data, Json, Query, Redirect};
 use poem::{handler, Body, IntoResponse, Response, Result};
 
 use serde_json::{json, Value};
 
-use crate::api::openapi::commons::PlexoOpenAPISpecs;
 use crate::core::app::Core;
-use crate::core::config::{COOKIE_SESSION_NAME, COOKIE_SESSION_SAME_SITE, COOKIE_SESSION_SECURE}; // COOKIE_SESSION_DOMAIN,
-use crate::errors::app::PlexoAppError;
+use crate::core::config::{COOKIE_SESSION_DOMAIN, COOKIE_SESSION_NAME, COOKIE_SESSION_SAME_SITE, COOKIE_SESSION_SECURE};
 
-use super::resources::PlexoAuthToken;
 use super::{
-    commons::{get_token_from_cookie, get_token_from_headers, GITHUB_USER_API},
+    commons::GITHUB_USER_API,
     resources::{EmailLoginParams, EmailRegisterParams, GithubCallbackParams},
 };
 
@@ -129,25 +126,6 @@ pub async fn github_callback_handler(plexo_core: Data<&Core>, params: Query<Gith
 }
 
 #[handler]
-pub fn logout() -> impl IntoResponse {
-    let mut session_token_cookie = Cookie::named(COOKIE_SESSION_NAME.to_string());
-
-    session_token_cookie.set_value_str("");
-    session_token_cookie.set_http_only(true);
-    session_token_cookie.set_secure(true);
-    session_token_cookie.set_same_site(SameSite::Strict);
-    session_token_cookie.set_expires(Utc::now() - Duration::days(1));
-    session_token_cookie.set_path("/");
-
-    Redirect::moved_permanent("/")
-        .with_header("Set-Cookie", session_token_cookie.to_string())
-        .with_header(CACHE_CONTROL, "no-cache, no-store, must-revalidate")
-        .with_header(PRAGMA, "no-cache")
-        .with_header(EXPIRES, "0")
-        .into_response()
-}
-
-#[handler]
 pub async fn email_basic_login_handler(plexo_engine: Data<&Core>, params: Json<EmailLoginParams>) -> impl IntoResponse {
     let Ok(Some(member)) = plexo_engine.0.engine.get_member_by_email(params.email.clone()).await else {
         return Response::builder()
@@ -204,30 +182,21 @@ pub async fn email_basic_login_handler(plexo_engine: Data<&Core>, params: Json<E
         _ => SameSite::None,
     };
 
+    let cookie_domain = COOKIE_SESSION_DOMAIN.to_string();
+
     session_token_cookie.set_value_str(session_token.clone());
     session_token_cookie.set_http_only(true);
     session_token_cookie.set_secure(cookie_secure);
     session_token_cookie.set_same_site(cookie_same_site);
     session_token_cookie.set_expires(Utc::now() + Duration::days(7));
     session_token_cookie.set_path("/");
+    session_token_cookie.set_domain(cookie_domain);
 
     Response::builder()
         .status(StatusCode::OK)
         .header(SET_COOKIE, session_token_cookie.to_string())
         .header("Content-Type", "application/json")
         .body(Body::from_json(json!({ "access_token": session_token })).unwrap())
-}
-
-fn _get_token(headers: &HeaderMap) -> Result<PlexoAuthToken, PlexoAppError> {
-    if let Some(token) = get_token_from_headers(headers) {
-        return Ok(token);
-    }
-
-    if let Some(token) = get_token_from_cookie(headers) {
-        return Ok(token);
-    }
-
-    Err(PlexoAppError::NotFoundPoemError(poem::error::NotFoundError))
 }
 
 #[handler]
@@ -316,6 +285,20 @@ pub async fn logout_handler() -> Result<Response> {
 }
 
 #[handler]
-pub async fn get_open_api_specs(specs: Data<&PlexoOpenAPISpecs>) -> Result<String> {
-    Ok(specs.0.clone().0)
+pub async fn logout() -> impl IntoResponse {
+    let mut session_token_cookie = Cookie::named(COOKIE_SESSION_NAME.to_string());
+
+    session_token_cookie.set_value_str("");
+    session_token_cookie.set_http_only(true);
+    session_token_cookie.set_secure(true);
+    session_token_cookie.set_same_site(SameSite::Strict);
+    session_token_cookie.set_expires(Utc::now() - Duration::days(1));
+    session_token_cookie.set_path("/");
+
+    Redirect::moved_permanent("/")
+        .with_header("Set-Cookie", session_token_cookie.to_string())
+        .with_header(CACHE_CONTROL, "no-cache, no-store, must-revalidate")
+        .with_header(PRAGMA, "no-cache")
+        .with_header(EXPIRES, "0")
+        .into_response()
 }
