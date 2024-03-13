57becb3d1d53086b3258a1c2de205a2cb3ae86f4
Bregy Malpartida <bregy@minsky.cc>
2024-03-12 21:15:29 -0500

    feat: refactor task mutation methods to register changes

--- src/api/graphql/operations/tasks.rs
+++ src/api/graphql/operations/tasks.rs
@@ -50,10 +50,11 @@ impl TasksGraphQLMutation {
     // TODO: It's possible that this method may not work correctly, as the owner_id is being ignored by async_graphql
     async fn create_task(&self, ctx: &Context<'_>, input: CreateTaskInput) -> Result<Task> {
         let (core, member_id) = extract_context(ctx)?;
-        let saved_input = input.clone();
 
         let mut input = input;
         input.owner_id = member_id;
+
+        let saved_input = input.clone();
 
         let task = core.engine.create_task(input).await?;
         let saved_task = task.clone();
@@ -95,31 +96,123 @@ impl TasksGraphQLMutation {
         let mut input = input;
         input.tasks.iter_mut().for_each(|task| task.owner_id = member_id);
 
-        core.engine
-            .create_tasks(input)
-            .await
-            .map_err(|err| async_graphql::Error::new(err.to_string()))
-            .map(|tasks| tasks.into_iter().map(|task| task.into()).collect())
+        let saved_input = input.clone();
+
+        let tasks = core.engine.create_tasks(input).await?;
+        let saved_tasks = tasks.clone();
+
+        // .map_err(|err| async_graphql::Error::new(err.to_string()))
+        // .map(|tasks| tasks.into_iter().map(|task| task.into()).collect())
+
+        tasks.iter().for_each(|task| {
+            let engine = core.engine.clone();
+            let input = saved_input.clone();
+            let task = task.clone();
+
+            task::spawn(async move {
+                let change = engine
+                    .create_change(
+                        CreateChangeInputBuilder::default()
+                            .owner_id(member_id)
+                            .resource_id(task.id)
+                            .operation(ChangeOperation::Create)
+                            .resource_type(ChangeResourceType::Task)
+                            .diff_json(
+                                serde_json::to_string(&json!({
+                                    "input": input,
+                                    "result": task,
+                                }))
+                                .unwrap(),
+                            )
+                            .build()
+                            .unwrap(),
+                    )
+                    .await
+                    .unwrap();
+
+                println!("change registered: {} | {}", change.operation, change.resource_type);
+            });
+        });
+
+        Ok(saved_tasks.into_iter().map(|task| task.into()).collect())
     }
 
     async fn update_task(&self, ctx: &Context<'_>, id: Uuid, input: UpdateTaskInput) -> Result<Task> {
-        let (core, _member_id) = extract_context(ctx)?;
+        let (core, member_id) = extract_context(ctx)?;
 
-        core.engine
-            .update_task(id, input)
-            .await
-            .map_err(|err| async_graphql::Error::new(err.to_string()))
-            .map(|task| task.into())
+        let saved_input = input.clone();
+
+        let task = core.engine.update_task(id, input).await?;
+
+        let task = task.clone();
+        let saved_task = task.clone();
+        let engine = core.engine.clone();
+
+        // .map_err(|err| async_graphql::Error::new(err.to_string()))
+        // .map(|task| task.into())
+
+        tokio::spawn(async move {
+            let change = engine
+                .create_change(
+                    CreateChangeInputBuilder::default()
+                        .owner_id(member_id)
+                        .resource_id(task.id)
+                        .operation(ChangeOperation::Update)
+                        .resource_type(ChangeResourceType::Task)
+                        .diff_json(
+                            serde_json::to_string(&json!({
+                                "input": saved_input,
+                                "result": task,
+                            }))
+                            .unwrap(),
+                        )
+                        .build()
+                        .unwrap(),
+                )
+                .await
+                .unwrap();
+
+            println!("change registered: {} | {}", change.operation, change.resource_type);
+        });
+
+        Ok(saved_task.into())
     }
 
     async fn delete_task(&self, ctx: &Context<'_>, id: Uuid) -> Result<Task> {
         let (core, _member_id) = extract_context(ctx)?;
 
-        core.engine
-            .delete_task(id)
-            .await
-            .map_err(|err| async_graphql::Error::new(err.to_string()))
-            .map(|task| task.into())
+        let task = core.engine.delete_task(id).await?;
+        let saved_task = task.clone();
+
+        // .map_err(|err| async_graphql::Error::new(err.to_string()))
+        // .map(|task| task.into())
+
+        let engine = core.engine.clone();
+
+        tokio::spawn(async move {
+            let change = engine
+                .create_change(
+                    CreateChangeInputBuilder::default()
+                        .owner_id(task.owner_id)
+                        .resource_id(task.id)
+                        .operation(ChangeOperation::Delete)
+                        .resource_type(ChangeResourceType::Task)
+                        .diff_json(
+                            serde_json::to_string(&json!({
+                                "result": task,
+                            }))
+                            .unwrap(),
+                        )
+                        .build()
+                        .unwrap(),
+                )
+                .await
+                .unwrap();
+
+            println!("change registered: {} | {}", change.operation, change.resource_type);
+        });
+
+        Ok(saved_task.into())
     }
 }
 
